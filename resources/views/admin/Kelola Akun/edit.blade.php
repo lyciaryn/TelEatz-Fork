848570efee63b048168a486e7ab0311afda68788
nadyamauliida <nadyamauliida19@gmail.com>
2025-05-21 13:16:45 +0700

    kelola akun

---
+++ resources/views/admin/Kelola Akun/edit.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Edit User</h1>
+    <form action="{{ route('users.update', $user->id) }}" method="POST">
+        @csrf @method('PUT')
+        <div class="mb-3">
+            <label>Nama</label>
+            <input type="text" name="name" class="form-control" value="{{ $user->name }}" required>
+        </div>
+        <div class="mb-3">
+            <label>Email</label>
+            <input type="email" name="email" class="form-control" value="{{ $user->email }}" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Update</button>
+        <a href="{{ route('users.index') }}" class="btn btn-secondary">Batal</a>
+    </form>
+</div>
+@endsection
