2bf528e8b7f8fcf4fd8de7785820f8b394cb7b00
nadyamauliida <nadyamauliida19@gmail.com>
2025-05-22 10:25:21 +0700

    kelola akun

--- resources/views/admin/Kelola Akun/edit.blade.php
+++ resources/views/admin/Kelola Akun/edit.blade.php
@@ -3,17 +3,21 @@
 @section('content')
 <div class="container">
     <h1>Edit User</h1>
-    <form action="{{ route('users.update', $user->id) }}" method="POST">
-        @csrf @method('PUT')
+    <form action="{{ route('users.store') }}" method="POST">
+        @csrf
         <div class="mb-3">
             <label>Nama</label>
-            <input type="text" name="name" class="form-control" value="{{ $user->name }}" required>
+            <input type="text" name="name" class="form-control" required>
         </div>
         <div class="mb-3">
             <label>Email</label>
-            <input type="email" name="email" class="form-control" value="{{ $user->email }}" required>
+            <input type="email" name="email" class="form-control" required>
         </div>
-        <button type="submit" class="btn btn-primary">Update</button>
+        <div class="mb-3">
+            <label>Password</label>
+            <input type="password" name="password" class="form-control" required>
+        </div>
+        <button type="submit" class="btn btn-success">Simpan</button>
         <a href="{{ route('users.index') }}" class="btn btn-secondary">Batal</a>
     </form>
 </div>
