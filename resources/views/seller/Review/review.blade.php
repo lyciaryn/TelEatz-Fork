dd11e35eab21d5a99437ed8e383c2d9b7d96c815
Habibaisy <ardanirayhanarya@gmail.com>
2026-06-08 18:26:25 +0700

    fix category

--- resources/views/seller/Review/review.blade.php
+++ resources/views/seller/Review/review.blade.php
@@ -95,7 +95,7 @@
                                         <p class="card-text">{{ Str::limit($item->deskripsi, 100) }}...</p>
                                         <br>
                                         <label for="kategori">Kategori</label>
-                                        <p class="card-text">{{ $item->category->nama_kategori }}</p>
+                                        <p class="card-text">{{ $item->category?->nama_kategori }}</p>
                                         <br>
                                         <label for="ketersediaan">Ketersediaan</label>
                                         <p class="card-text">{{ $item->is_avaialable == 1 ? 'Tidak Tersedia' : 'Tersedia' }}
